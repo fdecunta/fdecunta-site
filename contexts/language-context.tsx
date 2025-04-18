2b0902d47c7a6392aeeb308367a1ac761868534c
Facundo Decunta <fdecunta@agro.uba.ar>
2025-04-18 12:33:20 -0300

    Changed about me

--- contexts/language-context.tsx
+++ contexts/language-context.tsx
@@ -76,7 +76,7 @@ const translations = {
     view_all_publications: "View All Publications",
 
     // About page
-    bio_title: "Biography",
+    bio_title: "Bio",
     bio: "My name is Facundo Alcides Decunta. I am from Chivilcoy, a small town in Buenos Aires, Argentina. I studied Environmental Science at the University of Buenos Aires and graduated in 2021. That same year, I started my PhD program at the Faculty of Agronomy at the University of Buenos Aires.",
     bio_extended:
       `
@@ -91,19 +91,20 @@ I was an assistant teacher in the Ecology Department at the Faculty of Agronomy,
 
 ## Programming
 
-I like to write small tools that I need or that I think would be nice to have, and also because it is fun!
+I like to write small tools that I need or that I think would be nice to have, and also because it is fun.
 
 Most of my useful coding is done in R, as it is the language we use for statistics in ecology. I mostly write R code for data analysis, but I have also worked a bit with R packages. I contributed to [orchaRd](https://github.com/daniel1noble/orchaRd/), a package used for meta-analysis, by writing a couple of functions for conducting and plotting leave-one-out analyses.
 
 My other programming work is related to small personal projects. I like to write small tools for very specific problems, or just try to build something and see if it works. For this non-work programming, I usually use Python, but I have also done some C, Go, and written lots of small Bash scripts.
 
+
 ## Veganism
 
-I am vegan. Many people can decide every day what to eat. Some food options require animals to suffer and be killed; some options don't. Whenever possible, choose the latter. Nobody should pay for a product that requires suffering and pain to be produced. Also, veganism is a win-win-win ([Tilman & Clark, 2014](https://doi.org/10.1038/nature13959); [Clark et al. 2018](https://doi.org/10.1038/nature13959); [Clark et al. 2019](https://doi.org/10.1038/nature13959); [Scherer et al. 2019](https://doi.org/10.1038/nature13959): it is good for your health, good for the environment, and good for the animals.
+I am vegan. Many people can decide every day what to eat. Some food options require animals to suffer and be killed; some options don't. Whenever possible, choose the latter. Nobody should pay for a product that requires suffering and pain to be produced. Also, veganism is a win-win-win : it is good for your health, good for the environment, and good for the animals ([Tilman & Clark, 2014](https://doi.org/10.1038/nature13959); [Clark et al. 2018](https://doi.org/10.1038/nature13959); [Clark et al. 2019](https://doi.org/10.1038/nature13959); [Scherer et al. 2019](https://doi.org/10.1038/nature13959)).
 
 `,
     bio_additional:
-      "This is my bio additional",
+      "",
 
     // Publications page
     journal_articles: "Journal Articles",
@@ -156,12 +157,32 @@ I am vegan. Many people can decide every day what to eat. Some food options requ
     view_all_publications: "Ver Todas las Publicaciones",
 
     // About page
-    bio_title: "Biografía",
-    bio: "Mi bio",
+    bio_title: "Bio",
+    bio: "Me llamo Facundo Alcides Decunta. Soy de Chivilcoy, un pueblo del interior de la provincia de Buenos Aires, Argentina. Estudié Ciencias Ambientales en la Universidad de Buenos Aires y me gradué en 2021. Ese mismo año, empecé el doctorado en la Facultad de Agronomía de la UBA.",
     bio_extended:
-      "Mi bio extendida",
+      `
+## Investigación
+
+Estudio cómo las plantas interactúan con microorganismos simbióticos y herbívoros insectos. Mi modelo de estudio es la simbiosis entre pastos y hongos endófitos del género _Epichloë_, un mutualismo defensivo bien conocido. Actualmente, estudio esta simbiosis en el contexto de la herbivoría subterránea.
+
+## Docencia
+
+Fui ayudante de cátedra en el Departamento de Ecología de la Facultad de Agronomía, Universidad de Buenos Aires, desde 2018 hasta 2022. Luego, me mudé al Departamento de Métodos Cuantitativos y Sistemas de Información, donde estoy desde entonces. He estado enseñando a estudiantes de Ciencias Ambientales e Ingeniería Agronómica su primer curso de Estadística.
+
+## Programación
+
+Me gusta escribir pequeñas herramientas que necesito o que creo que sería bueno tener, y también porque es divertido.
+
+La mayor parte de mi programación útil la hago en R, porque es el lenguaje que usamos para análisis estadísticos en ecología. Principalmente escribo código en R para análisis de datos, pero también he trabajado un poco con paquetes de R. Contribuí a [orchaRd](https://github.com/daniel1noble/orchaRd/), un paquete utilizado para meta-análisis, escribiendo un par de funciones para realizar y graficar análisis leave-one-out.
+
+Mi otro trabajo de programación está relacionado con pequeños proyectos personales. Me gusta escribir herramientas para problemas muy específicos, o simplemente intentar construir algo y ver si funciona. Para esta programación no laboral, usualmente uso Python, pero también he hecho algo de C, Go y he escrito muchos scripts pequeños en Bash.
+
+## Veganismo
+
+Soy vegano. Muchas personas pueden decidir qué comer. Algunas opciones alimenticias requieren que los animales sufran y sean sacrificados; otras no. Siempre que sea posible, elige la segunda opción. Nadie debería pagar por un producto que requiere sufrimiento y dolor para ser producido. Además, el veganismo es un win-win-win: es bueno para tu salud, bueno para el ambiente y bueno para los animales ([Tilman & Clark, 2014](https://doi.org/10.1038/nature13959); [Clark et al. 2018](https://doi.org/10.1038/nature13959); [Clark et al. 2019](https://doi.org/10.1038/nature13959); [Scherer et al. 2019](https://doi.org/10.1038/nature13959)).
+`,
     bio_additional:
-      "Mi bio aun mas extendida",
+      "",
 
     // Publications page
     journal_articles: "Artículos de Revistas",
